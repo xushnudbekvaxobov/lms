4d101f392ade7d43e9b255ff307b004a4655c4c5
healer <xushnudbekvaxobov0421@gmail.com>
2026-05-04 00:52:00 +0500

    Build success: ready for Azure deployment

--- src/main/java/smartlms/service/serviceImpl/NotificationServiceImpl.java
+++ src/main/java/smartlms/service/serviceImpl/NotificationServiceImpl.java
@@ -2,13 +2,8 @@ package smartlms.service.serviceImpl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import smartlms.entity.NotificationEntity;
-import smartlms.repository.NotificationRepository;
 import smartlms.service.NotificationService;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
